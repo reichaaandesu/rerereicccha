c4471cfa78625b4662cd8141907a784735681474
加藤麗奈 <yurugigi@gmail.com>
2023-05-16 21:41:09 +0900

    1_3 修正

--- HelloWorld_KatoReina/src/sample/SelfIntroduction.java
+++ HelloWorld_KatoReina/src/sample/SelfIntroduction.java
@@ -3,10 +3,16 @@ package sample;
 public class SelfIntroduction {
 
 	public static void main(String[] args) {
+//	氏名
 		String name = "加藤 麗奈";
+//	年齢
 		int age = 26;
+//	趣味
 		String hobby = "カメラ、音楽鑑賞、ポケモン";
 
+		/*
+		 * 自己紹介文を出力する処理
+		 */
 		System.out.println("私の名前は" + name + "です。");
 		System.out.println("年齢は" + age + "歳です。");
 		System.out.println("趣味は" + hobby + "です。");
